cdb319e9b96bf28bbf0be76e17c13110b0c5f502
VilarCastroAndreaC <84359926+VilarCastroAndreaC@users.noreply.github.com>
2021-10-27 17:12:28 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComponentComponent } from './components/component/component.component';
+import { HomeComponent } from './components/home/home.component';
+import { Punto1Component } from './components/punto1/punto1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }, 
+  { path: 'punto1', component: Punto1Component },
+  { path: 'componente', component: ComponentComponent },
+  { path: '**', pathMatch:'full',redirectTo:'home' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './components/component/component.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { Punto1Component } from './components/punto1/punto1.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ComponentComponent,
+    FooterComponent,
+    HomeComponent,
+    Punto1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
